b56174bd3e95f247589b3a888e37ddd4baab370d
Edwin Ly <edwinjly@gmail.com>
2022-01-24 17:13:05 -0500

    Added dynamic URL for shortURL and EJS templates

--- express_server.js
+++ express_server.js
@@ -8,9 +8,22 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: "Hello World, I'm Edwin!" };
+  res.render("hello_world", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
