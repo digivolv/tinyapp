ff5d83f949ee6f03cd882dd7c5a1ddcf914b2c8f
Edwin Ly <edwinjly@gmail.com>
2022-01-24 15:29:11 -0500

    View engine now EJS

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -17,6 +19,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
