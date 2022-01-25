837f3983d506c8ba628f041fcbf5cb744616a64a
Edwin Ly <edwinjly@gmail.com>
2022-01-25 12:35:32 -0500

    Implemented delete URL and update longURL features

--- express_server.js
+++ express_server.js
@@ -4,8 +4,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 8080;
 
+//./node_modules/.bin/nodemon -L express_server.js//
+
 app.set("view engine", "ejs");
 
+//ROUTE DEFINITIONS
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -26,15 +29,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send(`OK ${generateRandomString()}`);
-});
-
+//dynamic page, path depending on shortURL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -43,18 +43,41 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: "Hello World, I'm Edwin!" };
-//   res.render("hello_world", templateVars);
-// });
+//Update your express server so that when it receives a POST request to /urls
+// it responds with a redirection to /urls/:shortURL, where shortURL is the random string we generated.
+
+app.post("/urls", (req, res) => {
+  console.log("req.body: ", req.body);
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  // console.log(urlDatabase[newShortURL]);
+  // console.log(urlDatabase);
+  res.redirect(`/urls/${newShortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (!longURL) {
+    res.send("404: Key not found!");
+  }
+  res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL/", (req, res) => {
+  console.log(urlDatabase[req.params.shortURL]);
+  console.log("req.body: ", req.body);
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls/${req.params.shortURL}`);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 
 app.get("/set", (req, res) => {
   const a = 1;
