3374c264cd73cccddd53b2cad2081ca1020a97b8
Edwin Ly <edwinjly@gmail.com>
2022-01-26 16:46:06 -0500

    added login and registration functionality.  cookies now store userID instead of username

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const users = require("./data/users.json");
 app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = 8080;
+const PORT = 8081;
 
+// console.log(users);
 //./node_modules/.bin/nodemon -L express_server.js//
 app.use(cookieParser());
 app.set("view engine", "ejs");
@@ -15,18 +17,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
+// const users = {
+//   userRandomID: {
+//     id: "userRandomID",
+//     email: "user@example.com",
+//     password: "purple-monkey-dinosaur",
+//   },
+//   user2RandomID: {
+//     id: "user2RandomID",
+//     email: "user2@example.com",
+//     password: "dishwasher-funk",
+//   },
+// };
 
 function generateRandomString() {
   let result = "";
@@ -37,76 +39,51 @@ function generateRandomString() {
   }
   return result;
 }
+// users[req.cookies["user"]].email,
+
+//returns user ID if email exists
+function checkIfEmailExists(inputEmail) {
+  for (const user in users) {
+    if (inputEmail === users[user].email) {
+      return user;
+    }
+  }
+  return false;
+}
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user"]],
+  };
   res.render("urls_index", templateVars);
 });
 
-//form
+app.post("/urls", (req, res) => {
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
+});
+
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user"]] };
   res.render("urls_new", templateVars);
 });
 
-//dynamic page, path depending on shortURL
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.cookies);
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user"]],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
 });
 
-app.post("/register", (req, res) => {
-  // console.log("entering this endpoint");
-  // console.log(req.body.id);
-  // console.log(req.body.password);
-  // console.log(req.body.email);
-  let userID = generateRandomString();
-
-  users[userID] = {
-    username: req.body.username,
-    firstName: req.body.firstName,
-    email: req.body.email,
-    password: req.body.password,
-  };
-  res.cookie(userID, "username");
-  res.redirect("/urls");
-});
-
-app.get("/register", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-    shortURL: req.params.shortURL,
-  };
-
-  res.render("register", templateVars);
-});
-//Update your express server so that when it receives a POST request to /urls
-// it responds with a redirection to /urls/:shortURL, where shortURL is the random string we generated.
-
-app.post("/urls", (req, res) => {
-  console.log("req.body: ", req.body);
-  let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = req.body.longURL;
-  // console.log(urlDatabase[newShortURL]);
-  // console.log(urlDatabase);
-  res.redirect(`/urls/${newShortURL}`);
-});
-
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  // console.log(req.body.username);
-  // console.log(username);
-  res.redirect(`/urls`);
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect(`/urls`);
+app.post("/urls/:shortURL/", (req, res) => {
+  // console.log(urlDatabase[req.params.shortURL]);
+  // console.log("req.body: ", req.body);
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect(`/urls/${req.params.shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -117,30 +94,77 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user"]],
+    shortURL: req.params.shortURL,
+  };
+
+  res.render("register", templateVars);
+});
+
+app.post("/register", (req, res) => {
+  if (!req.body.firstName || !req.body.email || !req.body.password) {
+    res.sendStatus(400).send("All input fields required");
+  }
+  if (checkIfEmailExists(req.body.email)) {
+    res
+      .sendStatus(400)
+      .send(
+        "Email has already been previously registered with another account"
+      );
+  }
+  let user_id = generateRandomString();
+  users[user_id] = {
+    id: user_id,
+    firstName: req.body.firstName,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  console.log(users);
+  res.cookie("user", user_id);
+  res.redirect("/urls");
+});
+
+//Update your express server so that when it receives a POST request to /urls
+// it responds with a redirection to /urls/:shortURL, where shortURL is the random string we generated.
+app.get("/login", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    user: users[req.cookies["user"]],
+  };
+  res.render("login", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  // console.log(req.body.email);
+  let user_id = checkIfEmailExists(req.body.email);
+
+  // console.log("req.body.password,", req.body.password);
+  // console.log("user.password,", user_id.password);
+  if (users[user_id].password === req.body.password) {
+    // console.log("user:", user);
+    res.cookie("user", user_id);
+  } else if (!checkIfEmailExists(req.body.email)) {
+    res.sendStatus(403).send("Email is not found");
+  }
+  // res.cookie("user", req.body.user);
+  res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user");
+  res.redirect(`/urls`);
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
-app.post("/urls/:shortURL/", (req, res) => {
-  console.log(urlDatabase[req.params.shortURL]);
-  console.log("req.body: ", req.body);
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect(`/urls/${req.params.shortURL}`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
